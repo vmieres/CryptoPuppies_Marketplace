206b2c33ddf9b5af3d3263c5c455e07ad60a43ee
victormieres <mieresvictor01@gmail.com>
2021-03-06 13:01:12 -0500

    updated files

--- PupperCoin_files/Crowdsale_PC.sol
+++ PupperCoin_files/Crowdsale_PC.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.5;
+pragma solidity ^0.5.0;
 
 import "./PupperCoin.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
--- PupperCoin_files/PupperCoin.sol
+++ PupperCoin_files/PupperCoin.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.5;
+pragma solidity ^0.5.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
